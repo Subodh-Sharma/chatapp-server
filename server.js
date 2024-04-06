9ee3c5a2e8962d829c9cb05967b3f58d35e97671
Subodh <sharma2000subodh@gmail.com>
2024-04-06 22:18:19 +0530

    add home route

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use("/api/users", userRoutes);
 
 // app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get("/", (req, res) => {
+	res.send("Welcome to SUBODH CHAT APP SERVER");
+});
 
 server.listen(PORT, () => {
 	connectToMongoDB();
